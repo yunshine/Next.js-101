cc9a3c36c7c15917be89ac42e2c2f25a81edca67
Yun Chung <yun.chung@mac.com>
2020-08-30 14:45:05 +0900

    Created Fetching & Server API project

--- Fetching & Server API/.next/static/chunks/webpack.js
+++ Fetching & Server API/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5d06d28caa020dd771cb";
+/******/ 	var hotCurrentHash = "9a1b33d240f9d1d75e24";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
