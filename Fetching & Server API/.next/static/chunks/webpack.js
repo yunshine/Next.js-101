a3e371317941899df7d530dece48c2f4441bcef9
Yun Chung <yun.chung@mac.com>
2020-08-31 16:04:30 +0900

    Able to access and display the comments for each corresponding post in the API and created a new comments component to display it

--- Fetching & Server API/.next/static/chunks/webpack.js
+++ Fetching & Server API/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "39006821194f4595bc4f";
+/******/ 	var hotCurrentHash = "56de4c273945732e735e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- Fetching & Server API/pages/post.js
+++ Fetching & Server API/pages/post.js
@@ -5,13 +5,23 @@ const Post = (props) => (
   <div>
     <h1>Post Page. These are the comments for post #{props.id}</h1>
     {props.comments.map((comment) => (
-      <p>{comment.email}</p>
+      // <p>{comment.email}</p>
+      <Comment {...comment} key={comment.id} />
     ))}
   </div>
 );
+
+const Comment = ({ email, body }) => (
+  <div>
+    <h5>{email}</h5>
+    <p>{body}</p>
+    <br></br>
+  </div>
+);
+
 Post.getInitialProps = async ({ query }) => {
   const res = await axios.get(
-    'https://jsonplaceholder.typicode.com/comments?postId=1'
+    `https://jsonplaceholder.typicode.com/comments?postId=${query.id}`
   );
   const { data } = res;
   console.log(data[0]);
